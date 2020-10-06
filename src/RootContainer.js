246d325f0aac15bbe5b7f5bc3614e7e7b7a987d7
AdrianBZG <adrianrodriguezbazaga@gmail.com>
2020-10-06 10:43:02 +0100

    Clean

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -2,7 +2,40 @@ import React from 'react';
 import queryData from './query';
 import Controls from './controls';
 import Plotly from 'react-plotly.js';
-import { getColors } from './colors';
+
+// Colors
+const colors = alpha => [
+	`rgb(57, 119, 175, ${alpha})`,
+	`rgb(239, 133, 54, ${alpha})`,
+	`rgb(81, 157, 62, ${alpha})`,
+	`rgb(197, 57, 50, ${alpha})`,
+	`rgb(141, 107, 184, ${alpha})`,
+	`rgb(133, 88, 77, ${alpha})`,
+	`rgb(213, 126, 190, ${alpha})`,
+	`rgb(127, 127, 127, ${alpha})`,
+	`rgb(189, 188, 69, ${alpha})`,
+	`rgb(87, 188, 204, ${alpha})`,
+	`rgb(235, 68, 31, ${alpha})`,
+	`rgb(181, 252, 129, ${alpha})`,
+	`rgb(129, 252, 226, ${alpha})`,
+	`rgb(129, 207, 252, ${alpha})`,
+	`rgb(176, 129, 252, ${alpha})`,
+	`rgb(252, 129, 245, ${alpha})`,
+	`rgb(208, 133, 111, ${alpha})`,
+	`rgb(93, 224, 137, ${alpha})`,
+	`rgb(1, 255, 216, ${alpha})`,
+	`rgb(52, 35, 202, ${alpha})`,
+	`rgb(90, 15, 103, ${alpha})`
+];
+
+export const getColors = (alpha = 1, length = 500) =>
+	Array(length)
+		.fill(null)
+		.map((_, i) => {
+			const cs = colors(alpha);
+			return cs[i % cs.length];
+		});
+//
 
 class RootContainer extends React.Component {
 	constructor(props) {
@@ -72,10 +105,6 @@ class RootContainer extends React.Component {
 			dataset: value
 		};
 
-		console.log("OnChange");
-		console.log(name);
-		console.log(value);
-
 		this.setState({ filterOptions: filterOptionsNew });
 
 		this.queryDataWithFilters(filterOptionsNew);
@@ -85,21 +114,12 @@ class RootContainer extends React.Component {
 	render() {
 		var data = [];
 
-		if(this.state.data) {
-			console.log("hey1");
-			console.log(this.state.data);
-			console.log(this.state.data.length);
-		}
-
 		if(this.state.data && this.state.data.length > 0) {
 			var colors = getColors(0.9, this.state.data.length);
 			
-			for(var i = 0; i < this.state.data.length; i++){
+			for(var i = 0; i < this.state.data.length; i++) {
 				var dataForThisTrace = [];
 				var geneName = this.state.data[i][0].gene.symbol;
-				console.log("Epale");
-				console.log(this.state.data[i]);
-				console.log(this.state.data[i].length);
 				var sum = 0;
 				for(var j = 0; j < this.state.data[i].length; j++) {
 					if(this.state.filterOptions["dataset"] == "Achilles Gene Effect") {
@@ -111,13 +131,10 @@ class RootContainer extends React.Component {
 					}
 				}
 
-				console.log(dataForThisTrace.length);
-				console.log(sum);
 				var trace = {
 					x: dataForThisTrace,
 					name: geneName,
 					marker: {color: colors[i]},
-					//yaxis: 'y2',
 					type: 'histogram'
 				};
 
@@ -125,58 +142,90 @@ class RootContainer extends React.Component {
 
 				data.push(trace);
 			}
+
+			var layout = {
+				showlegend: true,
+				autosize: true,
+				height: 300,
+				margin: {t: 25},
+				hovermode: 'closest',
+				bargap: 0,
+				xaxis: {
+					title: {
+						text: 'Gene effect score',
+						font: {
+							family: 'sans-serif',
+							size: 18,
+							color: '#7f7f7f'
+						}
+					},
+					showgrid: false,
+					zeroline: false
+				},
+				yaxis: {
+					title: {
+						text: '# Cell lines',
+						font: {
+							family: 'sans-serif',
+							size: 18,
+							color: '#7f7f7f'
+						}
+					},
+					showgrid: false,
+					zeroline: false
+				},
+				shapes: [{
+					type: 'line',
+					x0: -1,
+					y0: 0,
+					x1: -1,
+					yref: 'paper',
+					y1: 1,
+					line: {
+						color: 'red',
+						width: 1.5,
+						dash: 'dot'
+					}
+				},
+				{
+					type: 'line',
+					x0: 0,
+					y0: 0,
+					x1: 0,
+					yref: 'paper',
+					y1: 1,
+					line: {
+						color: 'black',
+						width: 1.5,
+						dash: 'dot'
+					}
+				}]
+			};
+	
+			return (
+				<div className="rootContainer">
+					<Controls
+						updateFilters={this.updateFilters}
+						filters={this.state.filterOptions}
+					/>
+					<span className="chart-title">DepMap Screens Visualization</span>
+					
+					<Plotly
+						data={data}
+						layout={layout}
+						style={{ width: '100%' }}
+					/>
+				</div>
+			);
+		} else {
+			return (
+				<div className="rootContainer">
+					No data available!
+				</div>
+			);
 		}
 
-		var layout = {
-			showlegend: true,
-			autosize: true,
-			height: 300,
-			margin: {t: 25},
-			hovermode: 'closest',
-			bargap: 0,
-			xaxis: {
-				//domain: [0, 0.85],
-				showgrid: false,
-				zeroline: false
-			},
-			yaxis: {
-				//domain: [0, 0.85],
-				showgrid: false,
-				zeroline: false
-			},
-			shapes: [{
-				type: 'line',
-				x0: -1,
-				y0: 0,
-				x1: -1,
-				yref: 'paper',
-				y1: 1,
-				line: {
-					color: 'black',
-					width: 1.5,
-					dash: 'dot'
-				}
-			}]
-		};
-
-		console.log("epa3:");
-		console.log(data);
-
-		return (
-			<div className="rootContainer">
-				<Controls
-					updateFilters={this.updateFilters}
-					filters={this.state.filterOptions}
-				/>
-				<span className="chart-title">DepMap Screens Visualization</span>
-				
-				<Plotly
-					data={data}
-					layout={layout}
-					style={{ width: '100%' }}
-				/>
-			</div>
-		);
+		
 	}
 }
 
